01a47edc1fa40055fe8d6dc0b9a244eab89379bc
brian.dooley@feedhenry.com <brian.dooley@feedhenry.com>
2014-07-07 15:13:15 +0000

    update file /cloud/main.js

--- cloud/main.js
+++ cloud/main.js
@@ -3,6 +3,7 @@ var util = require('util');
 exports.myServerFunction = function (params, callback) {
     console.log("In myServerFunction");
 	console.log(JSON.stringify(params));
-	return callback(undefined, JSON.stringify(params));
+  return callback(undefined, {});
+//	return callback(undefined, JSON.stringify(params));
 //	return callback(undefined, 'In myServerFunction');
 };
